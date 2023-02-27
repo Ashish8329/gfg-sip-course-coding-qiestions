75e57386a456b7289eb3248214309d73d4578af7
Ashish Auti <117579752+Ashish8329@users.noreply.github.com>
2023-02-27 22:14:30 +0530

    Added solution - LeetHub

---
+++ Check String - GFG/check-string.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        bool check (string s)
+        {
+        	//code here.
+        	bool ans=true;
+        	for(int i=1;i<s.length();i++){
+        	    if(s[i]!=s[i- 1]){
+        	        ans= false;
+        	        break;
+        	    }
+        	    
+        }
+        return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+		string s;
+		cin >> s;
+		Solution ob;
+		if (ob.check (s))
+			cout << "YES\n";
+		else 
+			cout << "NO\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
