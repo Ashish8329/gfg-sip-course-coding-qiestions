6199559b27e0f6daea6ed8298345e2f15978e5ef
Ashish Auti <117579752+Ashish8329@users.noreply.github.com>
2023-03-04 07:08:42 +0530

    Added solution - LeetHub

---
+++ Java Strings | Set 1 - GFG/java-strings-set-1.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string conRevstr(string S1, string S2) {
+        // code here
+        string s = S1 + S2;
+        string a;
+        int  n = s.length();
+        
+        for(int i=n;i>=0;i--){
+            a += s[i];
+            
+            
+            
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S1, S2;
+        cin >> S1;
+        cin >> S2;
+        Solution ob;
+        cout << ob.conRevstr(S1, S2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
